0dd4e4e90707766d3c61eaf471418b02fe8aaa00
Alexander Ryazanov <alryaz@xavux.com>
2021-02-01 12:17:42 +0300

    Attempt at fixing configuration

--- custom_components/mosenergosbyt/__init__.py
+++ custom_components/mosenergosbyt/__init__.py
@@ -46,7 +46,11 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_USERNAME): cv.string,
                 vol.Required(CONF_PASSWORD): cv.string,
                 vol.Optional(CONF_ACCOUNTS, default=[]): vol.Any(
-                    vol.All(cv.ensure_list, [cv.string]),
+                    vol.All(
+                        cv.ensure_list,
+                        [cv.string],
+                        lambda x: dict.fromkeys(x, True)
+                    ),
                     {cv.string: vol.Any(
                         vol.All(cv.ensure_list, [cv.string]),
                         vol.All(cv.boolean, True)
